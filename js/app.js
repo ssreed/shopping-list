6f7f7b74d280db5f0df87ebd23a027a3adf066d2
Sri <srisreed@gmail.com>
2013-06-20 17:09:17 -0700

    checked for duplicates, updated addition/removal from array

--- js/app.js
+++ js/app.js
@@ -9,36 +9,77 @@ $(function(){
     var submitButton = $('#submit-button');
     var removeButton = $('#remove-button');
     var inputArray = [];
+    var j = 0;
 
-    var appendItem = function(){
-        list.append('<li> ' + input.val() + ' </li>');
-        addToarray(input.val());
+    var appendItem = function() {
+        list.append('<li> ' + input.val() + ' </li>'); 
+        //addToarray(input.val());
+        //console.log(inputArray); 
     };
 
     var addToarray = function(arg) {
         inputArray.push(arg);
     };
-    
-    submitButton.on('click', function(){
-        appendItem();
+
+    var isDuplicate = function(arg) {
+        var flag = false;       
+        for(var i = 0; i < inputArray.length; i++)
+        {
+            if($.trim(arg) === inputArray[i])
+            {
+                flag = true;
+            }
+        }
+        return flag;
+    };
+
+    var add2array = function() {
+        inputArray = [];
+        $('ul li').each(function(){
+            inputArray.push($.trim($(this).text()).replace(/[^\w\s]/gi, '')); 
+            //console.log($.trim($(this).text()));
+            //console.log($.trim($(this).text()).replace(/[^\w\s]/gi, ''));
+        });
         console.log(inputArray);
+    };
+
+    submitButton.on('click', function() {
+        if(input.val() !== '' && isDuplicate(input.val()) !== true)
+        {
+            appendItem();
+            add2array();
+        }
     });
 
-    input.keypress(function(e){
+    input.keypress(function(e) {
         if(e.which === 13)
         {
             e.preventDefault();
-            appendItem();
-            console.log(inputArray);
+            if(input.val() !== '' && isDuplicate(input.val()) !== true)
+            {
+                appendItem();
+                j++;
+                add2array();
+                // j++;
+                // console.log(j);
+                // for(var i = j; i <= inputArray.length; i++)
+                // {
+                //      list.append('<li id="' + j + '"  > ' + input.val() + ' </li>'); 
+                // }
+            }
         }
     });
 
     doc.on('click', 'li', function() {
         $(this).toggleClass('clicked');
+        //console.log(j);
         return false;
     });
 
-    removeButton.on('click', function(){
+    removeButton.on('click', function() {
+        //console.log(ul.find('.clicked'));
+        //var index = ul.find('.clicked').id;
+        //inputArray.splice(index, 1);
         ul.find('.clicked').remove();
     });
 
